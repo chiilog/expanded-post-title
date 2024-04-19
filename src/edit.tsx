cbc50ab3cbffc43ce02931544e19feb7bde1a23f
chiilog <purgexpurge@gmail.com>
2024-04-19 14:07:32 +0900

    見出しレベルとテキスト配置追加

--- src/edit.tsx
+++ src/edit.tsx
@@ -1,4 +1,6 @@
-import { RichText, useBlockProps } from '@wordpress/block-editor';
+import classnames from 'classnames';
+
+import { RichText, useBlockProps, useBlockEditingMode, BlockControls, HeadingLevelDropdown, AlignmentControl } from '@wordpress/block-editor';
 import type { BlockEditProps } from "@wordpress/blocks";
 import { useEntityProp } from '@wordpress/core-data';
 import { useEffect } from "@wordpress/element";
@@ -7,11 +9,22 @@ import './editor.scss';
 
 type BlockAttributes = {
 	title: string;
+	level: number;
 	userEdited: boolean;
+	textAlign: string;
 }
 
-export default function Edit( { attributes: { title, userEdited }, setAttributes, context: { postType, postId } }: BlockEditProps< BlockAttributes > ) {
-	const blockProps = useBlockProps();
+export default function Edit( { attributes: { title, level, userEdited, textAlign }, setAttributes, context: { postType, postId } }: BlockEditProps< BlockAttributes > ) {
+	const blockProps = useBlockProps(
+		{
+			className: classnames( {
+				[ `has-text-align-${ textAlign }` ]: textAlign,
+			} ),
+		}
+	);
+	const TagName = level === 0 ? 'p' : `h${ level }`;
+	const blockEditingMode = useBlockEditingMode();
+
 	const [ rawTitle = '' ] = useEntityProp(
 		'postType',
 		// @ts-ignore
@@ -28,17 +41,34 @@ export default function Edit( { attributes: { title, userEdited }, setAttributes
 	}, [ rawTitle ] );
 
 	return (
-		<div { ...blockProps }>
-			<RichText
-				tagName="h1"
-				value={ title }
-				onChange={ ( value ) => {
-					setAttributes( {
-						title: value,
-						userEdited: true
-					} );
-				} }
-			/>
-		</div>
+		<>
+			{ blockEditingMode === 'default' && (
+				<BlockControls group="block">
+					<HeadingLevelDropdown
+						value={ level }
+						onChange={ ( newLevel: number ) =>
+							setAttributes( { level: newLevel } )
+						}
+					/>
+					<AlignmentControl
+						value={ textAlign }
+						onChange={ ( newAlign: string ) => {
+							setAttributes( { textAlign: newAlign } );
+						} }
+					/>
+				</BlockControls>
+			) }
+			<TagName {...blockProps}>
+				<RichText
+					value={title}
+					onChange={(value) => {
+						setAttributes({
+							title: value,
+							userEdited: true
+						});
+					}}
+				/>
+			</TagName>
+		</>
 	);
 }
--- src/render.php
+++ src/render.php
@@ -6,7 +6,14 @@
  *
  * @see https://github.com/WordPress/gutenberg/blob/trunk/docs/reference-guides/block-api/block-metadata.md#render
  */
+
+$tagName = $attributes['level'] === 0 ? 'p' : 'h' . $attributes['level'];
+
+$classes = array();
+if ( isset( $attributes['textAlign'] ) ) {
+	$classes[] = 'has-text-align-' . $attributes['textAlign'];
+}
 ?>
-<div <?php echo get_block_wrapper_attributes(); ?>>
-	<h1><?php echo wp_kses_post( $attributes['title'] ); ?></h1>
-</div>
+<<?php echo $tagName; ?> <?php echo get_block_wrapper_attributes( array( 'class' => implode( ' ', $classes ) ) ); ?>>
+	<?php echo wp_kses_post( $attributes['title'] ); ?>
+</<?php echo $tagName; ?>>
