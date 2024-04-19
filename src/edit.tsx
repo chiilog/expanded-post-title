33ce68d3052b5fd188f3d0c20b8f3b84fa36008c
chiilog <purgexpurge@gmail.com>
2024-04-19 11:58:25 +0900

    編集したかどうかを保持

--- src/edit.tsx
+++ src/edit.tsx
@@ -1,29 +1,16 @@
 import { RichText, useBlockProps } from '@wordpress/block-editor';
-import type {BlockEditProps} from "@wordpress/blocks";
+import type { BlockEditProps } from "@wordpress/blocks";
 import { useEntityProp } from '@wordpress/core-data';
 import { useEffect } from "@wordpress/element";
 
-/**
- * Lets webpack process CSS, SASS or SCSS files referenced in JavaScript files.
- * Those files can contain any CSS code that gets applied to the editor.
- *
- * @see https://www.npmjs.com/package/@wordpress/scripts#using-css
- */
 import './editor.scss';
 
 type BlockAttributes = {
 	title: string;
+	userEdited: boolean;
 }
 
-/**
- * The edit function describes the structure of your block in the context of the
- * editor. This represents what the editor will render when the block is used.
- *
- * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-edit-save/#edit
- *
- * @return {Element} Element to render.
- */
-export default function Edit( { attributes: { title }, setAttributes, context: { postType, postId } }: BlockEditProps< BlockAttributes > ) {
+export default function Edit( { attributes: { title, userEdited }, setAttributes, context: { postType, postId } }: BlockEditProps< BlockAttributes > ) {
 	const blockProps = useBlockProps();
 	const [ rawTitle = '' ] = useEntityProp(
 		'postType',
@@ -33,7 +20,7 @@ export default function Edit( { attributes: { title }, setAttributes, context: {
 		postId
 	);
 	useEffect(() => {
-		if ( title === '' || title === rawTitle ) {
+		if ( ! userEdited ) {
 			setAttributes( {
 				title: rawTitle,
 			} );
@@ -48,6 +35,7 @@ export default function Edit( { attributes: { title }, setAttributes, context: {
 				onChange={ ( value ) => {
 					setAttributes( {
 						title: value,
+						userEdited: true
 					} );
 				} }
 			/>
